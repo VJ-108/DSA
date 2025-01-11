58a6c2a94ed636706712bd20225ff784f968b27c
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-01-11 19:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (58.01%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,124 +1,80 @@
 class Solution {
 public:
-    // bool canFill(vector<string> &curr,int row,int col,int n){
-    //     int duprow = row;
-    //     int dupcol = col;
-
-    //     //upper diagonal
-    //     while(row>=0 && col>=0){
-    //         if (curr[row][col]=='Q') return false;
-    //         row--;
-    //         col--;
+    // bool isSafe(vector<string> &board,int n,int row,int col){
+    //     for (int tempr=row-1;tempr>=0;tempr--){
+    //         if (board[tempr][col]=='Q') return false;
     //     }
-    //     col = dupcol;
-    //     row = duprow;
 
-    //     //left row
-    //     while(col>=0){
-    //         if (curr[row][col]=='Q') return false;
-    //         col--;
+    //     int tempr=row-1,tempc=col-1;
+    //     while(tempr>=0 && tempc>=0){
+    //         if (board[tempr][tempc]=='Q') return false;
+    //         tempr--;
+    //         tempc--;
     //     }
-    //     col = dupcol;
-    //     row = duprow;
 
-    //     //lower diagonal
-    //     while(row<n && col>=0){
-    //         if (curr[row][col]=='Q') return false;
-    //         row++;
-    //         col--;
+    //     tempr=row-1,tempc=col+1;
+    //     while(tempr>=0 && tempc<n){
+    //         if (board[tempr][tempc]=='Q') return false;
+    //         tempr--;
+    //         tempc++;
     //     }
 
     //     return true;
     // }
-    // void back(vector<vector<string>> &ans,vector<string> &curr,int n,int col){
-    //     if (col == n){
-    //         ans.push_back(curr);
+
+    // void helper(vector<vector<string>> &ans,vector<string> &board,int n,int row, int nQ){
+    //     if (nQ==0){
+    //         ans.push_back(board);
     //         return;
     //     }
-
-    //     for (int row=0;row<n;row++){
-    //         if (canFill(curr,row,col,n)){
-    //             curr[row][col] = 'Q';
-    //             back(ans,curr,n,col+1);
-    //             curr[row][col] = '.';
+    //     for (int col=0;col<n;col++){
+    //         if (isSafe(board,n,row,col)){
+    //             board[row][col]='Q';
+    //             helper(ans,board,n,row+1,nQ-1);
+    //             board[row][col]='.';
     //         }
     //     }
-    // }
-
-    // void back(vector<vector<string>> &ans,vector<string> &curr,int n,vector<int> &leftRow, vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int col){
-    //     if (col == n){
-    //         ans.push_back(curr);
-    //         return;
-    //     }
-    //     for (int row = 0;row<n;row++){
-    //         if (leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
-    //             curr[row][col] = 'Q';
-    //             leftRow[row]=1;
-    //             lowerDiagonal[row+col]=1;
-    //             upperDiagonal[n-1+col-row]=1;
-    //             back(ans,curr,n,leftRow,upperDiagonal,lowerDiagonal,col+1);
-    //             curr[row][col] = '.';
-    //             leftRow[row]=0;
-    //             lowerDiagonal[row+col]=0;
-    //             upperDiagonal[n-1+col-row]=0;
-    //         }
-    //     }
-
     // }
     // vector<vector<string>> solveNQueens(int n) {
-    //     vector<vector<string>> ans;
-    //     vector<string> curr(n);
-    //     string s(n,'.');
-    //     for (int i=0;i<n;i++) curr[i]=s;
-    //     vector<int> leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
-    //     back(ans,curr,n,leftRow,upperDiagonal,lowerDiagonal,0);
+    //     vector < vector < string >> ans;
+    //     vector < string > board(n);
+    //     string s(n, '.');
+    //     for (int i = 0; i < n; i++) {
+    //         board[i] = s;
+    //     }
+    //     helper(ans,board,n,0,n);
     //     return ans;
     // }
 
-    
-    bool isSafe(vector<string> &board,int n,int row,int col){
-        for (int tempr=row-1;tempr>=0;tempr--){
-            if (board[tempr][col]=='Q') return false;
-        }
 
-        int tempr=row-1,tempc=col-1;
-        while(tempr>=0 && tempc>=0){
-            if (board[tempr][tempc]=='Q') return false;
-            tempr--;
-            tempc--;
-        }
 
-        tempr=row-1,tempc=col+1;
-        while(tempr>=0 && tempc<n){
-            if (board[tempr][tempc]=='Q') return false;
-            tempr--;
-            tempc++;
-        }
-
-        return true;
-    }
-
-    void helper(vector<vector<string>> &ans,vector<string> &board,int n,int row, int nQ){
-        if (nQ==0){
-            ans.push_back(board);
+    void back(vector<vector<string>> &ans,vector<string> &curr,int n,vector<int> &leftRow, vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int col){
+        if (col == n){
+            ans.push_back(curr);
             return;
         }
-        for (int col=0;col<n;col++){
-            if (isSafe(board,n,row,col)){
-                board[row][col]='Q';
-                helper(ans,board,n,row+1,nQ-1);
-                board[row][col]='.';
+        for (int row = 0;row<n;row++){
+            if (leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
+                curr[row][col] = 'Q';
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                back(ans,curr,n,leftRow,upperDiagonal,lowerDiagonal,col+1);
+                curr[row][col] = '.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
         }
+
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector < vector < string >> ans;
-        vector < string > board(n);
-        string s(n, '.');
-        for (int i = 0; i < n; i++) {
-            board[i] = s;
-        }
-        helper(ans,board,n,0,n);
+        vector<vector<string>> ans;
+        vector<string> curr(n);
+        string s(n,'.');
+        for (int i=0;i<n;i++) curr[i]=s;
+        vector<int> leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
+        back(ans,curr,n,leftRow,upperDiagonal,lowerDiagonal,0);
         return ans;
     }
 };
