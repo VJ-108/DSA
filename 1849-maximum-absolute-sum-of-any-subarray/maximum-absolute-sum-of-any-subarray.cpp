e30bc497ada59707d98464eabac7d736fcdaa9c1
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-02-26 15:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
+        /*
         int mini_neg=INT_MAX,maxi_pos=INT_MIN,sum=0;
         int ans = INT_MIN;
         for (int i=0;i<nums.size();i++){
@@ -17,5 +18,19 @@ public:
             }
         }
         return ans;
+        */
+
+
+        // Kadane’s Algorithm can be modified to solve this efficiently.
+        // Instead of just tracking the maximum subarray sum, we also track the minimum subarray sum.
+        // The answer is |maxSum - minSum|, since flipping the sign of a negative subarray maximizes the absolute sum.
+        int mini_neg=0,maxi_pos=0,sum=0;
+        int ans = INT_MIN;
+        for (int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if (sum > maxi_pos) maxi_pos = sum;
+            if (sum < mini_neg) mini_neg = sum;
+        }
+        return abs(maxi_pos - mini_neg);
     }
 };
