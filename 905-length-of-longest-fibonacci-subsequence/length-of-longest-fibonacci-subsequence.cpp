038e67c83bb2ae900a401622e80bb7676b8d6b06
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-02-27 13:22:28 +0530

    Time: 263 ms (82.48%) | Memory: 15.6 MB (87.82%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    void rec(vector<int>& arr, unordered_map<int, int>& mpp, int a, int b, int& cnt) {
-        if (b >= arr.size() || mpp.find(arr[a] + arr[b]) == mpp.end()) {
-            return;
-        }
-        cnt++;
-        rec(arr, mpp, b, mpp[arr[a] + arr[b]], cnt);
-    }
+    // void rec(vector<int>& arr, unordered_map<int, int>& mpp, int a, int b, int& cnt) {
+    //     if (b >= arr.size() || mpp.find(arr[a] + arr[b]) == mpp.end()) {
+    //         return;
+    //     }
+    //     cnt++;
+    //     rec(arr, mpp, b, mpp[arr[a] + arr[b]], cnt);
+    // }
 
     int lenLongestFibSubseq(vector<int>& arr) {
+        // unordered_map<int, int> mpp;
+        // for (int i = 0; i < arr.size(); i++) {
+        //     mpp[arr[i]] = i;
+        // }
+        // int ans = 0;
+        // for (int i = 0; i < arr.size(); i++) {
+        //     for (int j = i + 1; j < arr.size(); j++) {
+        //         int cnt = 2;
+        //         rec(arr, mpp, i, j, cnt);
+        //         ans = max(ans, cnt);
+        //     }
+        // }
+        // return ans > 2 ? ans : 0;
+
         unordered_map<int, int> mpp;
         for (int i = 0; i < arr.size(); i++) {
             mpp[arr[i]] = i;
@@ -16,11 +30,18 @@ public:
         int ans = 0;
         for (int i = 0; i < arr.size(); i++) {
             for (int j = i + 1; j < arr.size(); j++) {
+                int prev = arr[i],cur = arr[j];
+                int nxt = prev + cur;
                 int cnt = 2;
-                rec(arr, mpp, i, j, cnt);
-                ans = max(ans, cnt);
+                while (mpp.find(nxt) != mpp.end()){
+                    cnt++;
+                    prev = cur;
+                    cur = nxt;
+                    nxt = prev + cur;
+                    ans = max(ans,cnt);
+                }
             }
         }
-        return ans > 2 ? ans : 0;
+        return ans;
     }
 };
