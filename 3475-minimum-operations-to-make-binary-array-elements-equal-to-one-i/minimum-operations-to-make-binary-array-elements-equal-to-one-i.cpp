40782f56f5a3bc06c2926066953dbe590dd676f5
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-03-19 19:20:17 +0530

    Time: 9 ms (40.49%) | Memory: 139.9 MB (74.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    void flip(vector<int>&nums,int i){
-        nums[i]=nums[i]==0?1:0;
-        nums[i+1]=nums[i+1]==0?1:0;
-        nums[i+2]=nums[i+2]==0?1:0;
-    }
     int minOperations(vector<int>& nums) {
-        int l=0;
-        int cnt=0;
-        while(l<nums.size()){
-            while(l<nums.size() && nums[l]!=0) l++;
-            if (l+2 < nums.size()){
-                flip(nums,l);
-                cnt++;
-            }else break;
+        int n = nums.size(), k = 0;
+        for (int i = 0; i <= n - 3; i++) {
+            if (nums[i] == 0) {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                k++;
+            }
         }
-        for (auto it: nums){
-            if (it==0) return -1;
+        for (int num : nums) {
+            if (num == 0) return -1;
         }
-        return cnt;
+        return k;
     }
 };
