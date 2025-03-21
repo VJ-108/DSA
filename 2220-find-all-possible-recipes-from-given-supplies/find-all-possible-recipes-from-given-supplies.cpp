084d3bd177c3187d920c8884e9517a78897e529b
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-03-21 18:07:21 +0530

    Time: 36 ms (93.18%) | Memory: 101.1 MB (89.12%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
+    // vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+    //     unordered_set<string> hashset;
+    //     vector<string> output;
+    //     for (auto it: supplies){
+    //         hashset.insert(it);
+    //     }
+    //     bool isNewOutputAdded;
+    //     do{
+    //         isNewOutputAdded = false;
+    //         for (int i=0;i<recipes.size();i++){
+    //             if (hashset.find(recipes[i])!=hashset.end()) continue;
+    //             bool isfulfilled = true;
+    //             for (auto it: ingredients[i]){
+    //                 if (hashset.find(it)==hashset.end()){
+    //                     isfulfilled = false;
+    //                     break;
+    //                 }
+    //             }
+    //             if (isfulfilled){
+    //                 hashset.insert(recipes[i]);
+    //                 output.push_back(recipes[i]);
+    //                 isNewOutputAdded = true;
+    //             }
+    //         }
+    //     }while(isNewOutputAdded);
+    //     return output;
+    // }
+
+
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_set<string> hashset;
-        vector<string> output;
-        for (auto it: supplies){
-            hashset.insert(it);
-        }
-        bool isNewOutputAdded;
-        do{
-            isNewOutputAdded = false;
-            for (int i=0;i<recipes.size();i++){
-                if (hashset.find(recipes[i])!=hashset.end()) continue;
-                bool isfulfilled = true;
-                for (auto it: ingredients[i]){
-                    if (hashset.find(it)==hashset.end()){
-                        isfulfilled = false;
-                        break;
-                    }
-                }
-                if (isfulfilled){
-                    hashset.insert(recipes[i]);
-                    output.push_back(recipes[i]);
-                    isNewOutputAdded = true;
+        int n = recipes.size();
+        unordered_set<string> supp(supplies.begin(), supplies.end());
+        vector<int> deg(n, 0);
+        unordered_map<string, vector<int>> adj;
+
+        for (int i = 0; i < n; i++) {
+            for (const string& s : ingredients[i]) {
+                if (!supp.count(s)) {
+                    adj[s].push_back(i);
+                    deg[i]++;
                 }
             }
-        }while(isNewOutputAdded);
-        return output;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+            if (deg[i] == 0) q.push(i);
+
+        vector<string> ans;
+        while (!q.empty()) {
+            const int i = q.front();
+            q.pop();
+            auto s=recipes[i];
+            ans.push_back(s);
+            for (auto j : adj[s]) {
+                if (--deg[j] == 0) q.push(j);
+            }
+        }
+        return ans;
     }
 };
