e4e68fc8fcb709aa15432b8f2514327732e1b961
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-03-24 18:25:37 +0530

    Time: 77 ms (34.69%) | Memory: 126.5 MB (59.18%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -3,15 +3,32 @@ public:
     int countDays(int days, vector<vector<int>>& meetings) {
         if (meetings.empty()) return days;
         sort(meetings.begin(), meetings.end());
-        int prev_end = 0;
-        int start = -1;
-        for (auto it: meetings){
-            start = max(it[0],prev_end+1);
-            int end = it[1];
-            days = days - max(0,end-start+1);
-            prev_end = max(prev_end,end);
+        
+        int totalBusy = 0;
+        int start = meetings[0][0];
+        int end = meetings[0][1];
+
+        for (int i = 1; i < meetings.size(); ++i) {
+            if (meetings[i][0] <= end + 1) {
+                end = max(end, meetings[i][1]); 
+            } else {
+                totalBusy += (end - start + 1);
+                start = meetings[i][0];
+                end = meetings[i][1];
+            }
         }
-        return days;
+        totalBusy += (end - start + 1);
+        return max(0, days - totalBusy);
+
+        // int prev_end = 0;
+        // int start = -1;
+        // for (auto it: meetings){
+        //     start = max(it[0],prev_end+1);
+        //     int end = it[1];
+        //     days = days - max(0,end-start+1);
+        //     prev_end = max(prev_end,end);
+        // }
+        // return days;
     }
 
 };
