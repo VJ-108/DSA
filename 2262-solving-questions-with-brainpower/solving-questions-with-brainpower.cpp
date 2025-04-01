5e6bb8ef389ffc7094d250b40e8eeb45568d5254
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-01 17:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 119.8 MB (58.89%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -13,7 +13,20 @@ public:
         return dp[i] = max(skip,take);
     }
     long long mostPoints(vector<vector<int>>& questions) {
-        vector<long long> dp(questions.size(),-1);
-        return func(questions,0,dp);
+        // vector<long long> dp(questions.size(),-1);
+        // return func(questions,0,dp);
+
+        vector<long long> dp(questions.size()+1,0);
+        dp[0]=0;
+        for (int i = questions.size() - 1; i >= 0; --i) {
+            long long skip = dp[i + 1];
+            long long take = questions[i][0];
+            if (i + questions[i][1] + 1 < questions.size()) {
+                take += dp[i + questions[i][1] + 1];
+            }
+            dp[i] = max(skip, take);
+        }
+        
+        return dp[0];
     }
 };
