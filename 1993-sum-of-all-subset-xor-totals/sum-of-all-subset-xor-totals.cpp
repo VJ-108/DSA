573a1849e14d5131ca29692f1d2472030a9f1ee4
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-05 10:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (71.99%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -8,36 +8,36 @@ public:
     }
     int subsetXORSum(vector<int>& nums) {
         //Recursion - O(2^n),O(n)
-        // return func(nums,0,0);
+        return func(nums,0,0);
 
 
-        // //Power set - O(2^n * n), O(1)
-        // int sum=0;
-        // int subset = 1<<nums.size();
-        // for (int num=0;num<subset;num++){
-        //     int xo = 0;
-        //     for (int i=0;i<nums.size();i++){
-        //         if (num & (1<<i)){
-        //             xo ^= nums[i];
-        //         }
-        //     }
-        //     sum += xo;
-        // }
-        // return sum;
+        //Power set - O(2^n * n), O(1)
+        int sum=0;
+        int subset = 1<<nums.size();
+        for (int num=0;num<subset;num++){
+            int xo = 0;
+            for (int i=0;i<nums.size();i++){
+                if (num & (1<<i)){
+                    xo ^= nums[i];
+                }
+            }
+            sum += xo;
+        }
+        return sum;
 
 
         //Didnt understand a single bit of this code but is O(n),O(1)
-        // Step 1: Compute bitwise OR of all elements
-        int or_all = 0;
-        for (int num : nums) {
-            or_all |= num;
-        }
+        // // Step 1: Compute bitwise OR of all elements
+        // int or_all = 0;
+        // for (int num : nums) {
+        //     or_all |= num;
+        // }
 
-        // Step 2: Compute number of subsets that include each bit
-        // 2^(n-1) subsets will include each bit
-        int multiplier = 1 << (nums.size() - 1);
+        // // Step 2: Compute number of subsets that include each bit
+        // // 2^(n-1) subsets will include each bit
+        // int multiplier = 1 << (nums.size() - 1);
 
-        // Step 3: Final result
-        return or_all * multiplier;
+        // // Step 3: Final result
+        // return or_all * multiplier;
     }
 };
