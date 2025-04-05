4f9c1df19000b03545290952473ef53bb5b4d2bb
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-05 10:53:50 +0530

    Time: 2 ms (44.04%) | Memory: 9.2 MB (71.99%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -7,19 +7,37 @@ public:
         return take + notTake;
     }
     int subsetXORSum(vector<int>& nums) {
+        //Recursion - O(2^n),O(n)
         // return func(nums,0,0);
 
-        int sum=0;
-        int subset = 1<<nums.size();
-        for (int num=0;num<subset;num++){
-            int xo = 0;
-            for (int i=0;i<nums.size();i++){
-                if (num & (1<<i)){
-                    xo ^= nums[i];
-                }
-            }
-            sum += xo;
+
+        // //Power set - O(2^n * n), O(1)
+        // int sum=0;
+        // int subset = 1<<nums.size();
+        // for (int num=0;num<subset;num++){
+        //     int xo = 0;
+        //     for (int i=0;i<nums.size();i++){
+        //         if (num & (1<<i)){
+        //             xo ^= nums[i];
+        //         }
+        //     }
+        //     sum += xo;
+        // }
+        // return sum;
+
+
+        //Didnt understand a single bit of this code but is O(n),O(1)
+        // Step 1: Compute bitwise OR of all elements
+        int or_all = 0;
+        for (int num : nums) {
+            or_all |= num;
         }
-        return sum;
+
+        // Step 2: Compute number of subsets that include each bit
+        // 2^(n-1) subsets will include each bit
+        int multiplier = 1 << (nums.size() - 1);
+
+        // Step 3: Final result
+        return or_all * multiplier;
     }
 };
