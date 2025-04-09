3d5f0155e78986852ef7c5e4d44e64336c232d87
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-09 17:54:42 +0530

    Time: 8 ms (84.28%) | Memory: 32.3 MB (57.55%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int op=0;
-        unordered_set <int> st;
-        for (auto it: nums){
-            if (it<k) return -1;
-            if (st.find(it)==st.end() && it>k){
-                st.insert(it);
-                op++;
-            }
-        }
-        return op;
+        // int op=0;
+        // unordered_set <int> st;
+        // for (auto it: nums){
+        //     if (it<k) return -1;
+        //     if (st.find(it)==st.end() && it>k){
+        //         st.insert(it);
+        //         op++;
+        //     }
+        // }
+        // return op;
+
+        //Bitset approach O(n), O(1)
+        int mini = *min_element(nums.begin(), nums.end());
+        if (mini < k) return -1;
+
+        bitset<100001> st;
+        for (int i : nums) if (i > k) st.set(i);    
+        return st.count();
     }
 };
