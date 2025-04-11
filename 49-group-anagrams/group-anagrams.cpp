44c817d05f319e2a48bd04f243b9219f0b508bc9
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-11 09:35:12 +0530

    Time: 76 ms (7.22%) | Memory: 37.4 MB (5.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,8 +3,16 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mpp;
         for (auto str : strs) {
-            string temp = str;
-            sort(temp.begin(), temp.end());
+            // string temp = str;
+            // sort(temp.begin(), temp.end());
+            vector<int> count(26, 0);
+            for (char c : str) {
+                count[c - 'a']++;
+            }
+            string temp;
+            for (int c : count) {
+                temp += '#' + to_string(c);
+            }
             mpp[temp].push_back(str);
         }
         vector<vector<string>> ans;
