e49201cd5a9bc8e437247d9bd9602a0002eded8f
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-18 19:17:44 +0530

    Time: 11 ms (42.66%) | Memory: 44.8 MB (74.49%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int l=0,r=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while (l < s.size() && r < g.size()) {
+            if (g[r] <= s[l]) r++;
+            l++;
+        }
+        return r;
+    }
+};
