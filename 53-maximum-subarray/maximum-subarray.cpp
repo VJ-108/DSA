0e5381fc03e5c54a36fa14b9b8bf22dd9344b7b8
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-21 11:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (18.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+    int helper(vector<int>& nums, int i, int j) {
+        if (i == j) {
+            return nums[i];
+        }
+
+        int mid = (i + j) / 2;
+
+        int sum = 0, leftMaxSUM = INT_MIN;
+        for (int l = mid; l >= i; l--) {
+            sum += nums[l];
+            leftMaxSUM = max(leftMaxSUM, sum);
+        }
+
+        int rightMaxSUM = INT_MIN;
+        sum = 0;
+        for (int l = mid + 1; l <= j; l++) {
+            sum += nums[l];
+            rightMaxSUM = max(rightMaxSUM, sum);
+        }
+
+        int maxLeftRight = max(helper(nums, i, mid), helper(nums, mid + 1, j));
+        return max(maxLeftRight, leftMaxSUM + rightMaxSUM);
+    }
     int maxSubArray(vector<int>& nums) {
+        // return helper(nums, 0, nums.size() - 1);
+
         if (nums.size()<1) return 0;
         int maxi = INT_MIN;
         int sum = 0;
