6ceaac18855642462a7857007d2ecef47a09095e
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-24 17:23:15 +0530

    Time: 1813 ms (5.22%) | Memory: 471.7 MB (16.87%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -9,20 +9,19 @@ public:
     int countCompleteSubarrays(vector<int>& nums) {
         set<int> st;
         for (auto it: nums) st.insert(it);
-        
         int cnt = 0;
+
         for (int i=0;i<nums.size();i++){
             set<int> temp;
             for (int j=i;j<nums.size();j++){
                 temp.insert(nums[j]);
-                if (temp.size()<st.size()) continue;
-                bool check = func(st,temp);
-                if (check){
+                if (temp.size()==st.size()){
                     cnt += nums.size()-j;
                     break;
                 }
             }
         }
+
         return cnt;
     }
 };
