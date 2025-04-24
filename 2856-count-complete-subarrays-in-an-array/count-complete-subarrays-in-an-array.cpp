dfcffd33a8606d8bac2049f698ee294bbc6a4d29
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-24 17:38:06 +0530

    Time: 21 ms (56.49%) | Memory: 42 MB (46.18%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -5,15 +5,28 @@ public:
         for (auto it: nums) st.insert(it);
         int cnt = 0;
 
-        for (int i=0;i<nums.size();i++){
-            set<int> temp;
-            for (int j=i;j<nums.size();j++){
-                temp.insert(nums[j]);
-                if (temp.size()==st.size()){
-                    cnt += nums.size()-j;
-                    break;
-                }
+        // for (int i=0;i<nums.size();i++){
+        //     set<int> temp;
+        //     for (int j=i;j<nums.size();j++){
+        //         temp.insert(nums[j]);
+        //         if (temp.size()==st.size()){
+        //             cnt += nums.size()-j;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        int left = 0,right = 0;
+        map<int,int> mpp;
+        while(right<nums.size()){
+            mpp[nums[right]]++;
+            while(mpp.size()==st.size()){
+                cnt += nums.size() - right;
+                mpp[nums[left]]--;
+                if (mpp[nums[left]]<=0) mpp.erase(nums[left]);
+                left++;
             }
+            right++;
         }
 
         return cnt;
