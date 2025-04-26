66f718fed5bd79d6c23a6333643b2c1c988b0d33
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-04-26 17:09:01 +0530

    Time: 49 ms (16.05%) | Memory: 53.4 MB (56.22%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -4,11 +4,11 @@
  */
 var expect = function(val) {
     return {
-        toBe: (value)=>{
+        toBe: function(value){
             if (value === val) return true;
             else throw new Error("Not Equal");
         },
-        notToBe: (value)=>{
+        notToBe: function(value){
             if (value !== val) return true;
             else throw new Error("Equal");
         },
