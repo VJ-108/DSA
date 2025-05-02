656980ab3eaaad72af28dfd9ea9ceb739c0d44b6
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-05-02 17:21:02 +0530

    Time: 112 ms (55.00%) | Memory: 126.3 MB (79.06%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    int dfs(int node, vector<vector<int>>& adj, vector<int>& informTime) {
+        int maxTime = 0;
+        for (int emp : adj[node]) {
+            maxTime = max(maxTime, dfs(emp, adj, informTime));
+        }
+        return informTime[node] + maxTime;
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<vector<int>> adj(n);
         for (int i=0;i<manager.size();i++){
             if (manager[i]==-1) continue;
             adj[manager[i]].push_back(i);
         }
+
+        return dfs(headID, adj, informTime);
 
         queue<pair<int,int>> q; // q -> {node,time}
         q.push({headID,0});
