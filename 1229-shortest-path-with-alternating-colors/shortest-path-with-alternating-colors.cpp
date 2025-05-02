7bb7583b6d0149a794d1ded11382cf36746fc91e
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-05-02 16:29:32 +0530

    Time: 72 ms (5.06%) | Memory: 36.3 MB (5.06%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -2,47 +2,38 @@ class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         // 0 -> Red , 1 -> Blue
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto it: redEdges) adj[it[0]].push_back({it[1],0});
-        for (auto it: blueEdges) adj[it[0]].push_back({it[1],1});
+        vector<vector<pair<int,int>>> adj(n); //O(4N)
 
-        vector<int> ans;
+        for (auto it: redEdges) adj[it[0]].push_back({it[1],0}); //O(2N)
+        for (auto it: blueEdges) adj[it[0]].push_back({it[1],1}); //O(2N)
+
+        vector<int> ans; //O(N)
         
-        for (int i=0;i<n;i++){
-            // int dist = 0;
+        for (int i=0;i<n;i++){ //O(N * N * (N*logN))
             bool isdone = false;
-            // vector<int> dis(n,1e9);
-            // dis[0] = 0;
             // pq -> {dist, {node,color_of_last_Edge}}
-            priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq; 
+            priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq; //O(2N)
             pq.push({0,{0,-1}});
             map<pair<int,int>,int> mpp;
-            while(!pq.empty()){
+            while(!pq.empty()){ // O(4N)
                 int d = pq.top().first;
                 int node = pq.top().second.first;
                 int col = pq.top().second.second;
                 pq.pop();
-                // if (d > dis[node]) continue;
                 mpp[{node,col}] = 1;
                 if (node == i){
                     isdone = true;
                     ans.push_back(d);
                     break;
                 };
-                // dist++;
-                for (auto [it,adj_col]: adj[node]){
+                for (auto [it,adj_col]: adj[node]){ // O(2N)
                     if (col != -1 && adj_col == col) continue;
                     if (mpp.find({it,adj_col})==mpp.end()){
-                        // if (dis[it] > dis[node] + 1){
-                            // dis[it] = dis[node] + 1;
-                            pq.push({d+1,{it,adj_col}});
-                        // }
+                        pq.push({d+1,{it,adj_col}}); //O(logN)
                     }
                 }
             }
-            if (!isdone) ans.push_back(-1);
-            // if (!isdone) dist = -1; 
-            // ans.push_back(dist);    
+            if (!isdone) ans.push_back(-1);  
         }
         return ans;
     }
