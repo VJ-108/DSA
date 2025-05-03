315863b008d54c3f416134cabe69773df9086b58
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-05-03 12:04:37 +0530

    Time: 3 ms (67.46%) | Memory: 115.3 MB (93.75%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -16,19 +16,41 @@ public:
 
         return {ntemp_key, ntemp_val};
     }
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        auto [ntops_key,ntops_val] = findMajor(tops);
-        auto [nbots_key,nbots_val] = findMajor(bottoms);
+
+    int check(int x, vector<int>& tops, vector<int>& bottoms) {
+        int swaps_top = 0, swaps_bottom = 0;
+        int n = tops.size();
         
-        int cnt_top = 0;
-        for (int i=0;i<tops.size();i++){
-            if (bottoms[i]==ntops_key && tops[i]!=ntops_key) cnt_top++;
+        for (int i = 0; i < n; ++i) {
+            if (tops[i] != x && bottoms[i] != x)
+                return -1;
+            if (tops[i] != x) swaps_top++;
+            if (bottoms[i] != x) swaps_bottom++;
         }
-        int cnt_bot = 0;
-        for (int i=0;i<bottoms.size();i++){
-            if (tops[i]==nbots_key && bottoms[i]!=nbots_key) cnt_bot++;
-        }
-        if (cnt_top + ntops_val != tops.size() && cnt_bot + nbots_val != bottoms.size()) return -1;
-        return min(cnt_top,cnt_bot);
+        
+        return min(swaps_top, swaps_bottom);
+    }
+
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        // auto [ntops_key,ntops_val] = findMajor(tops);
+        // auto [nbots_key,nbots_val] = findMajor(bottoms);
+        
+        // int cnt_top = 0;
+        // for (int i=0;i<tops.size();i++){
+        //     if (bottoms[i]==ntops_key && tops[i]!=ntops_key) cnt_top++;
+        // }
+        // int cnt_bot = 0;
+        // for (int i=0;i<bottoms.size();i++){
+        //     if (tops[i]==nbots_key && bottoms[i]!=nbots_key) cnt_bot++;
+        // }
+        // if (cnt_top + ntops_val != tops.size() && cnt_bot + nbots_val != bottoms.size()) return -1;
+        // return min(cnt_top,cnt_bot);
+
+        int val1 = tops[0];
+        int val2 = bottoms[0];
+        int res1 = check(val1, tops, bottoms);
+        if (res1 != -1)
+            return res1;
+        return check(val2, tops, bottoms);
     }
 };
