0a646622d1ceb48a04c38c7bc29929adebab57e2
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-05-05 11:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (55.01%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -14,12 +14,30 @@ public:
         return t[n] = (2*solve(n-1)%M + solve(n-3)%M)%M;
     }
     
-    int numTilings(int n) {
+    // int numTilings(int n) {
        
-        memset(t, -1, sizeof(t));
+    //     memset(t, -1, sizeof(t));
         
         
-        return solve(n);
+    //     return solve(n);
+        
+    // }
+    int numTilings(int n) {
+        if(n == 1 || n == 2)
+            return n;
+        
+        vector<int> t(n+1, 0);
+        
+        t[1] = 1;
+        t[2] = 2;
+        t[3] = 5;
+        
+        for(int i = 4; i<=n; i++) {
+            t[i] = (2*t[i-1]%M + t[i-3]%M) % M;
+        }
+        
+        return t[n];
+        
         
     }
 };
