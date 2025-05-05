f3c4637df56517324e9bc4ea0573a40e8cf65811
VJ-108 <119118343+VJ-108@users.noreply.github.com>
2025-05-05 19:15:12 +0530

    Time: 43 ms (14.71%) | Memory: 73.9 MB (14.76%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> indegree(n+1,0);
-        vector<int> outdegree(n+1,0);
+        // vector<int> indegree(n+1,0);
+        // vector<int> outdegree(n+1,0);
+        // for (auto it: trust){
+        //     // outdegree[it[0]]++;
+        //     // indegree[it[1]]++;
+
+        // }
+        // for (int i=1;i<indegree.size();i++){
+        //     if (indegree[i]==n-1 && outdegree[i]==0) return i;
+        // }
+
+        unordered_map<int,int> mpp;
+        for (int i=1;i<=n;i++) mpp[i]=0;
         for (auto it: trust){
-            outdegree[it[0]]++;
-            indegree[it[1]]++;
+            mpp[it[0]]--;
+            mpp[it[1]]++;
         }
-        for (int i=1;i<indegree.size();i++){
-            if (indegree[i]==n-1 && outdegree[i]==0) return i;
+        for (auto [key,val]: mpp){
+            if (val == n-1) return key;
         }
         return -1;
     }
